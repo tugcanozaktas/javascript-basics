08bf1f11a34223f26211bffb721ca55c1cc24525
Tuğcan Özaktaş <tugcanozaktas@hotmail.com>
2023-03-21 20:04:30 +0200

    solved numbers and strings

--- src/numbers.js
+++ src/numbers.js
@@ -1,45 +1,45 @@
-function add (a, b) {
-  // your code here
+function add(a, b) {
+  return a + b;
 }
 
-function subtract (a, b) {
-  // your code here
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply (a, b) {
-  // your code here
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide (a, b) {
-  // your code here
+function divide(a, b) {
+  return a / b;
 }
 
-function power (a, b) {
-  // your code here
+function power(a, b) {
+  return a ** b;
 }
 
-function round (a) {
-  // your code here
+function round(a) {
+  return Math.round(a);
 }
 
-function roundUp (a) {
-  // your code here
+function roundUp(a) {
+  return Math.ceil(a);
 }
 
-function roundDown (a) {
-  // your code here
+function roundDown(a) {
+  return Math.floor(a);
 }
 
-function absolute (a) {
-  // your code here
+function absolute(a) {
+  return Math.abs(a);
 }
 
-function quotient (a, b) {
-  // your code here
+function quotient(a, b) {
+  return Math.quotient(a, b);
 }
 
-function remainder (a, b) {
-  // your code here
+function remainder(a, b) {
+  return Math.remainder(a, b);
 }
 
 module.exports = {
--- src/strings.js
+++ src/strings.js
@@ -1,26 +1,34 @@
-function sayHello (string) {
-  // your code here
-};
+function sayHello(string) {
+  // eslint-disable-next-line prettier/prettier, prefer-template
+  const finalMessage = "Hello, " + string + "!";
+  return finalMessage;
+}
 
-function uppercase (string) {
-  // your code here
-};
+function uppercase(string) {
+  const upperCase = string.toUpperCase();
+  return upperCase;
+}
 
-function lowercase (string) {
-  // your code here
-};
+function lowercase(string) {
+  const lowerCase = string.toLowerCase();
+  return lowerCase;
+}
 
-function countCharacters (string) {
-  // your code here
-};
+function countCharacters(string) {
+  return string.length;
+}
 
-function firstCharacter (string) {
-  // your code here
-};
+function firstCharacter(string) {
+  return string[0];
+}
 
-function firstCharacters (string, n) {
-  // your code here
-};
+function firstCharacters(string, n) {
+  let word = '';
+  for (let i = 0; i < n; i += 1) {
+    word += string[i];
+  }
+  return word;
+}
 
 module.exports = {
   sayHello,
