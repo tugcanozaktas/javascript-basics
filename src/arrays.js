b4bc2c3d358c5915936f17c6ee78a456976ff4e0
Tuğcan Özaktaş <tugcanozaktas@hotmail.com>
2023-03-24 15:34:35 +0200

    solved arrays

--- src/arrays.js
+++ src/arrays.js
@@ -1,61 +1,122 @@
 const getNthElement = (index, array) => {
-  // your code here
+  if (index >= array.length) {
+    return array[index - array.length];
+  }
+  return array[index];
 };
 
 const arrayToCSVString = array => {
-  // your code here
+  return array.join(',');
 };
 
 const csvStringToArray = string => {
-  // your code here
+  return string.split(',');
 };
 
 const addToArray = (element, array) => {
-  // your code here
+  array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+  const newArray = array.concat(element);
+  return newArray;
 };
 
 const removeNthElement = (index, array) => {
-  // your code here
+  array.splice(index, 1);
 };
 
 const numbersToStrings = numbers => {
-  // your code here
+  for (let i = 0; i < numbers.length; i += 1) {
+    numbers.splice(i, 1, numbers[i].toString());
+  }
+  return numbers;
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+  for (let i = 0; i < strings.length; i += 1) {
+    strings.splice(i, 1, strings[i].toUpperCase());
+  }
+  return strings;
 };
 
 const reverseWordsInArray = strings => {
-  // your code here
+  for (let i = 0; i < strings.length; i += 1) {
+    strings.splice(
+      i,
+      1,
+      strings[i]
+        .split('')
+        .reverse()
+        .join('')
+    );
+  }
+  return strings;
 };
 
 const onlyEven = numbers => {
-  // your code here
+  const newNumbers = numbers.filter(function(number) {
+    return number % 2 === 0;
+  });
+  return newNumbers;
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    newArray.push(array[i]);
+  }
+  newArray.splice(index, 1);
+  return newArray;
 };
 
 const elementsStartingWithAVowel = strings => {
-  // your code here
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const vowelArray = [];
+  for (let i = 0; i < strings.length; i += 1) {
+    const word = strings[i];
+    if (vowels.includes(word[0]) || vowels.includes(word[0].toLowerCase())) {
+      vowelArray.push(strings[i]);
+    }
+  }
+  return vowelArray;
 };
 
 const removeSpaces = string => {
-  // your code here
+  const newString = string.split(' ').join('');
+  return newString;
 };
 
 const sumNumbers = numbers => {
-  // your code here
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    sum += numbers[i];
+  }
+  return sum;
 };
 
 const sortByLastLetter = strings => {
-  // your code here
+  const stringArray = [];
+
+  for (let i = 0; i < strings.length; i += 1) {
+    stringArray.push(
+      strings[i]
+        .split('')
+        .reverse()
+        .join('')
+    );
+  }
+  stringArray.sort();
+  const sortedArray = [];
+  for (let j = 0; j < stringArray.length; j += 1) {
+    sortedArray.push(
+      stringArray[j]
+        .split('')
+        .reverse()
+        .join('')
+    );
+  }
+  return sortedArray;
 };
 
 module.exports = {
